e37c0cf78dc1d52712398cf6af28dac6b9b4d8b5
Lucas Kray <vip.mr.lpv@mail.ru>
2022-08-31 19:21:29 +0300

    Home

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/*
+Console.WriteLine ("Введите число");
+string number = Console.ReadLine();
+
+if (number[0] == number[4] && number[1] == number[3])
+{
+    Console.WriteLine("Число палиндром");
+}
+else
+{
+    Console.WriteLine("Не является палиндромом");
+}
+Console.ReadLine();
+*/
+
+//Задача 2: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+/*
+            double x1 = 3, x2 = 2;
+            double y1 = 6, y2 = 1;
+            double z1 = 8, z2 = -7;
+
+
+            double dx = x2 - x1;
+            double dy = y2 - y1;
+            double dz = z2 - z1;
+            Console.WriteLine(Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2) + Math.Pow(dz, 2)));
+*/
+
+//Задача 3: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (double i = 1; i <= number; i++)
+{
+    Console.WriteLine(i * i * i);
+}
+
+Console.ReadLine();
+*/
